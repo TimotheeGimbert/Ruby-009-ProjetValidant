f79680162af31e1866c201efa45cb21e1c35af8b
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-05 14:36:04 +0200

    Adds exo 17 resolved

---
+++ exo_17.rb
@@ -0,0 +1,37 @@
+# FUNCTIONS 
+  
+def getInput() # Ask the user for an number of floors for the pyramid and returns it when it is a positiv integer below 25
+  puts "Combien d'étages veux-tu ?" # ask
+  print "> " # prompt
+  input = gets.chomp.to_i # convert to integer
+  while (Integer(input) === false || input <= 0 || input >= 26) do # While input is not a positiv integer below 25
+    puts "Merci de choisir un entier positif entre 1 et 25 ..." # ask
+    print "> " # prompt
+    input = gets.chomp.to_i # convert to integer
+  end
+  return input
+end
+
+def setPyramid(n) # Build full pyramid with n floors
+  for i in 1..n do # FOR each floor n
+    nbSpaces = n - i # calculate the number of free spaces to place first on the line n
+    for j in 0..nbSpaces do # FOR each free space of the line n
+        print " "
+    end
+    nbPoints = 1 + 2 * (i -1) # calculate the number of consecutiv drawings on the line n
+    for j in 1..nbPoints do # FOR each drawing spot on the line n
+        print "#"
+    end
+    puts # force a breakline at the end of the line n
+  end
+end
+
+# MAIN
+print "Salut, bienvenue dans ma super pyramide ! "
+nbFloors = getInput() # n is set to the returned value of the getInt() function
+puts "Voici une pyramide à #{nbFloors} étages :"
+setPyramid(nbFloors)
+
+
+
+
