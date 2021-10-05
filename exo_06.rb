3d1421b5311264f3da984530b0ba7d1826459a60
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-05 10:21:47 +0200

    Add exos to 7

---
+++ exo_06.rb
@@ -0,0 +1,6 @@
+puts "Give a positiv integer, not too big though.."
+repeat = gets.chomp.to_i
+
+for i in 2..repeat do
+    puts "Bonjour toi!"
+end
---
+++ exo_07.rb
@@ -0,0 +1,7 @@
+puts "Choisir un nombre entier positif"
+print "> "
+number = gets.chomp.to_i
+
+for i in 1..number do 
+    puts i
+end
